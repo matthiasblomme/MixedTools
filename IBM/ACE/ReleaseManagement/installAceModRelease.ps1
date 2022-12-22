15cddacbb5d8a67c624d7455528249ad1e31c443
matthiasblomme <b.matthias@gmail.com>
2022-12-22 09:32:33 +0100

    update

--- IBM/ACE/ReleaseManagement/installAceModRelease.ps1
+++ IBM/ACE/ReleaseManagement/installAceModRelease.ps1
@@ -11,7 +11,7 @@ function Unzip-ModRelease {
         $aceModDir
     )
 
-    $aceZip = $aceModDir + ".zip"
+    $aceZip = "$aceModDir.zip"
     $dir = [string](Get-Location)
     Write-Host "Unzipping $aceZip to $dir\$aceModDir"
     Expand-Archive $aceZip -DestinationPath $aceModDir -Force
@@ -27,8 +27,8 @@ function Install-ModRelease {
 
     #TODO: check if already installed
     Set-Location $aceModDir
-    $aceExe = "ACESetup" + $fixVersion + ".exe"
-    $logFile = $logBasePath + "\Ace_intall_" + $fixVersion + ".log"
+    $aceExe = "ACESetup$fixVersion.exe"
+    $logFile = "$logBasePath\Ace_intall_$fixVersion.log"
     $aceInstallCommand = $aceExe + " /install /quiet LICENSE_ACCEPTED=TRUE InstallFolder=`"" + $installDir + "`" /log " + $logFile
     Write-Host "Going to run $aceInstallCommand"
     Write-Host "(this may take some time) ..."
@@ -51,12 +51,42 @@ function Update-Mqsiprofile {
         $installDir
     )
 
-    #TODO: check if already present
-    $mqsiprofilePath = $installDir + "\server\bin\mqsiprofile.cmd"
-    Write-Host "Adding content to $mqsiprofilePath"
-    Add-Content -Path $mqsiprofilePath -value "rem  Custom values  ["
-    Add-Content -Path $mqsiprofilePath -value "set MQSI_FREE_MASTER_PARSERS=true"
-    Add-Content -Path $mqsiprofilePath -value "rem ]"
+    $mqsiprofilePath = "$installDir\server\bin\mqsiprofile.cmd"
+    if (Test-Path -Path $mqsiprofilePath) {
+        #TODO: check if already present
+        Write-Host "Adding content to $mqsiprofilePath"
+        Add-Content -Path $mqsiprofilePath -value "rem  Custom values  ["
+        Add-Content -Path $mqsiprofilePath -value "set MQSI_FREE_MASTER_PARSERS=true"
+        Add-Content -Path $mqsiprofilePath -value "rem ]"
+    } else {
+        Write-Host "Can't locate $mqsiprofilePath, stopping script"
+        exit 1
+    }
+
+}
+
+function Check-AceInstall {
+    param(
+        $fixVersion,
+        $installDir
+    )
+
+    $pwd = [string](Get-Location)
+    $checkScriptPath =  "$pwd\checkAceVersion.bat"
+    Write-Host "Creating temporary file $checkScriptPath"
+    New-Item -Path ./checkAceVersion.bat -Force
+    Add-Content -Path $checkScriptPath -value "call `"C:\Program Files\ibm\ACE\$fixVersion\server\bin\mqsiprofile.cmd`""
+    Add-Content -Path $checkScriptPath -value "call `"C:\Program Files\ibm\ACE\$fixVersion\server\bin\mqsiservice.exe`" -v"
+    $output = & $checkScriptPath
+    Remove-Item -Path $checkScriptPath
+    $selection = $output | Select-String "Version:" | select-String "$fixVersion"
+    Write-Host $selection
+    if ($selection -like "*$fixVersion*") {
+        Write-Host "$fixVersion appears to be properly installed, continuing ..."
+    } else {
+        Write-Host "Failed to verify $fixVersion installation, check installation and mqsiprofile.cmd"
+        exit 1
+    }
 }
 
 function Install-UDN {
@@ -67,7 +97,7 @@ function Install-UDN {
     $pwd = [string](Get-Location)
 
     if (Test-Path -Path "$pwd\udn\toolkit\") {
-        $pluginDir = $installDir + "\tools\plugins"
+        $pluginDir = "$installDir\tools\plugins"
         Write-Host "Copying from $pwd\udn\toolkit\ to $pluginDir"
         Copy-Item -Path $pwd\udn\toolkit\* -Destination $pluginDir -PassThru -Force
     } else {
@@ -75,7 +105,7 @@ function Install-UDN {
     }
 
     if (Test-Path -Path "$pwd\udn\runtime\") {
-        $jpluginDir = $installDir + "\server\jplugin"
+        $jpluginDir = "$installDir\server\jplugin"
         Write-Host "Copying from $pwd\udn\runtime\ to $jpluginDir"
         Copy-Item -Path $pwd\udn\runtime\* -Destination $jpluginDir -PassThru -Force
     } else {
@@ -92,7 +122,7 @@ function Install-SharedClasses {
     $pwd = [string](Get-Location)
 
     if (Test-Path -Path "$pwd\shared-classes") {
-        $sharedClassesDir = $runtimeBasePath + "\shared-classes"
+        $sharedClassesDir = "$runtimeBasePath\shared-classes"
         Write-Host "Copying from $pwd\shared-classes\ to $sharedClassesDir"
         Copy-Item -Path $pwd\shared-classes\* -Destination $sharedClassesDir -PassThru -Force
     } else {
@@ -108,7 +138,7 @@ function Install-JavaSecurity {
     $pwd = [string](Get-Location)
 
     if (Test-Path -Path "$pwd\security") {
-        $javaSecurityPath = $installDir + "\common\jdk\jre\lib\security"
+        $javaSecurityPath = "$installDir\common\jdk\jre\lib\security"
         Write-Host "Copying from $pwd\security to $javaSecurityPath"
         Copy-Item -Path $pwd\security\java.security -Destination $javaSecurityPath -PassThru -Force
     } else {
@@ -118,17 +148,16 @@ function Install-JavaSecurity {
 
 
 #run from C:\Users\ADM-BLMM_M\modrelease
-$aceModDir = "12.0-ACE-WINX64-" + $fixVersion
-$installDir = $installBasePath + "\" + $fixVersion
+$aceModDir = "12.0-ACE-WINX64-$fixVersion"
+$installDir = "$installBasePath\$fixVersion"
 
 Unzip-ModRelease -fixVersion $fixVersion -aceModDir $aceModDir
 
 Install-ModRelease -fixVersion $fixVersion -aceModDir $aceModDir -installDir $installDir -logBasePath $logBasePath
 
-#TODO check if version install ok via mqsiservice -v
-
 Update-Mqsiprofile -installDir $installDir
-#TODO check if profile still usable
+
+Check-AceInstall -fixVersion $fixVersion -installDir $installDir
 
 Install-UDN -installDir $installDir
 
