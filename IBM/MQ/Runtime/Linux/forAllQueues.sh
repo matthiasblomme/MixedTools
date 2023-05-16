6ca3609110eb9d58b92d1ee9c426e39d37465735
matthiasblomme <b.matthias@gmail.com>
2023-05-16 13:14:57 +0200

    update

--- IBM/MQ/Runtime/Linux/forAllQueues.sh
+++ IBM/MQ/Runtime/Linux/forAllQueues.sh
@@ -1,1 +1,5 @@
-echo dis ql(*) | runmqsc QmgrV10 | grep -oP "(?<=QUEUE\().*?(?=\))" | awk '{printf "ALTER QL(%s) MAXDEPTH(9999)\n",$1}'
+#print
+echo dis "ql(*)" | runmqsc QmgrV10 | grep -oP "(?<=QUEUE\().*?(?=\))" | awk '{printf "ALTER QL(%s) MAXDEPTH(9999)\n",$1}'
+
+#print with double quotes
+echo dis "ql(*" | runmqsc qmd01 | grep -oP '(?<=QUEUE\().*?(?=\))' | awk '{printf "ALTER QL(%s) CUSTOM('"'"'CAPEXPRY(123)'"'"')\n",$1}' |runmqsc qmd01
